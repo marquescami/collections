68ed6b7ed204d8731293409cbe628a331b07ccfb
marquescami <camilamsc@hotmail.com>
2022-03-18 17:54:38 -0300

    Java Collections

---
+++ src/Curso.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Curso {
+
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new ArrayList<Aula>();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getInstrutor() {
+		return instrutor;
+	}
+
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
+
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+}
---
+++ src/TestandoListas.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+
+        String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+        String curso2 = "Apache Camel";
+        String curso3 = "Certificacao Java SE 8 Programmer I";
+
+        ArrayList<String> cursos = new ArrayList<>();
+        cursos.add(curso1);
+        cursos.add(curso2);
+        cursos.add(curso3);        
+
+        System.out.println(cursos);
+        //System.out.println("O primeiro curso da lista eh o: "+ cursos.get(0)); 
+       // cursos.remove(1);
+       Collections.sort(cursos);
+       System.out.println(cursos);
+    }
+}
